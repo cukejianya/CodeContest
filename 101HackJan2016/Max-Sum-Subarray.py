8b5a6aac635de432c32797638dd1787f302fd1e3
Chinedum <cukejianya@gmail.com>
2016-01-22 23:29:35 -0500

    Update Max-Sum-Subarray.py

--- 101HackJan2016/Max-Sum-Subarray.py
+++ 101HackJan2016/Max-Sum-Subarray.py
@@ -1,13 +1,27 @@
 def find_max_sum(arr):
-    arr = arr.split(' 0 ')
-    for idx, sub_array in enumerate(arr):
-        
-        for elm in sub_array:
-            total = reduce(lambda x, y: x+y, [int(x) for x in sub_array.split()])
-            if idx == 0:
-                max_sum = total
+    arr = arr.split(' 0')
+    there_is_max = False
+    for sub_array in arr:
+        sub_array = [int(x) for x in sub_array.split()]
+        if not sub_array:
+            if there_is_max:
+                max_sum = max(0, max_sum)
             else:
-                max_sum = max(total, max_sum)
+                max_sum = 0
+                there_is_max = True
+            continue
+        for i in range(len(sub_array)):
+            for j in range(len(sub_array)-i):
+                min_array = sub_array[i:i+j+1]
+
+                total = reduce(lambda x, y: x+y, min_array)
+                if there_is_max:
+                    max_sum = max(total, max_sum)
+                else:
+                    max_sum = total
+                    there_is_max = True
+
+                print i, j, min_array, sub_array, max_sum
 
     return max_sum
 
