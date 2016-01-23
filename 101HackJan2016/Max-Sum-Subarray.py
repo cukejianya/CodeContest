cd4278acaea2a3d62a90c1f72f36a6f425cceac2
Chinedum <cukejianya@gmail.com>
2016-01-23 02:15:18 -0500

    Use Kadane's Algo to solve in O(n) time and O(1) space

--- 101HackJan2016/Max-Sum-Subarray.py
+++ 101HackJan2016/Max-Sum-Subarray.py
@@ -1,28 +1,15 @@
 def find_max_sum(arr):
     arr = arr.split(' 0')
-    there_is_max = False
+    max_sum = 0
     for sub_array in arr:
         sub_array = [int(x) for x in sub_array.split()]
-        if not sub_array:
-            if there_is_max:
-                max_sum = max(0, max_sum)
-            else:
-                max_sum = 0
-                there_is_max = True
-            continue
-        for i in range(len(sub_array)):
-            for j in range(len(sub_array)-i):
-                min_array = sub_array[i:i+j+1]
+        best = current = 0
+        for elm in sub_array:
+            current = max(current + elm, 0)
+            best = max(current, best)
+            print 'elm:', elm, 'current:', current, 'best:', best, 'max_sum:', max_sum
 
-                total = reduce(lambda x, y: x+y, min_array)
-                if there_is_max:
-                    max_sum = max(total, max_sum)
-                else:
-                    max_sum = total
-                    there_is_max = True
-
-                print i, j, min_array, sub_array, max_sum
-
+        max_sum = max(max_sum, best)
     return max_sum
 
 n = raw_input()
