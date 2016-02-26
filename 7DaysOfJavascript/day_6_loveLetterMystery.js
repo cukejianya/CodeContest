91b53798d19ca1e1f4c583430686e4fec9ac63be
Chinedum <cukejianya@gmail.com>
2016-02-26 00:50:10 -0500

    Finish chall 4 of day 6 in 7 days of JS

--- 7DaysOfJavascript/day_6_loveLetterMystery.js
+++ 7DaysOfJavascript/day_6_loveLetterMystery.js
@@ -1,7 +1,7 @@
  process.stdin.resume();
  process.stdin.setEncoding("ascii");
  var log = console.log;
- process.stdin.on("on", (input) => {
+ process.stdin.on("data", (input) => {
    this.input = this.input || "";
    this.input += input;
  });
@@ -17,9 +17,19 @@
 
 function range(num) {
   num = parseInt(num);
-  return Array.call(null, Array(num)).map((elm, idx) => idx);
+  return Array.apply(null, Array(num)).map((elm, idx) => idx);
 }
 
 function find_minimum(word) {
-  return null;
+  var operations = 0;
+  var wordLength = word.length;
+  var half = Math.floor(wordLength / 2);
+  for (var i = 0; i < half; i++) {
+    if(word[i] !== word[wordLength - i - 1]) {
+      operations += Math.abs(
+        word.charCodeAt(i) - word.charCodeAt(wordLength - i - 1)
+      );
+    }
+  }
+  return operations;
 }
